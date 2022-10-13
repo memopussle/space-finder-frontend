4364a160e29020924ab0ca478f1cb03ef84b3970
thugiang <xanhtham.cuc@gmail.com>
2022-10-13 22:40:53 +1300

    adding small css and react-router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,55 @@
 import React from "react";
 import { User } from "../model/Model";
 import { AuthService } from "../services/AuthService";
+import history from "../utils/history";
+import { Navbar } from "./Navbar";
 import { Login } from "./Login";
+import Home from "./Home";
+import Profile from "./Profile";
+import { Route, Router, Switch } from "react-router-dom";
 
 interface AppState {
   user: User | undefined;
 }
 
-
 class App extends React.Component<{}, AppState> {
   private authService: AuthService = new AuthService();
 
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      user: undefined,
+    };
+
+    this.setUser = this.setUser.bind(this);
+  }
+  private setUser(user: User) {
+    this.setState({
+      user: user,
+    });
+    console.log("setting the user!: " + user);
+  }
+
   render() {
     return (
-      <div>
-        <h1>App from class works!!!!</h1>
-        <Login authService={this.authService}/>
+      <div className="wrapper">
+        <div className="wrapper">
+          <Router history={history}>
+            <div>
+              <Navbar user={this.state.user} />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login">
+                  <Login
+                    authService={this.authService}
+                    setUser={this.setUser}
+                  />
+                </Route>
+                <Route exact path="/profile" component={Profile} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
       </div>
     );
   }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { AuthService } from "../services/AuthService";
+import {User} from"../model/Model"
 
 interface LoginProps {
-  authService: AuthService;
+    authService: AuthService;
+    setUser: (user: User) => void;
 }
 
 interface LoginState {
@@ -12,27 +14,71 @@ interface LoginState {
   loginSuccessful: boolean;
 }
 
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
 export class Login extends React.Component<LoginProps, LoginState> {
   state: LoginState = {
     userName: "",
     password: "",
     loginAttempted: false,
     loginSuccessful: false,
-    };
+  };
 
+  private setUserName(event: CustomEvent) {
+    this.setState({ userName: event.target.value });
+  }
+  private setPassword(event: CustomEvent) {
+    this.setState({ password: event.target.value });
+  }
 
+  private async handleSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    this.setState({ loginAttempted: true });
+    const result = await this.props.authService.login(
+      this.state.userName,
+      this.state.password
+    );
+
+    if (result) {
+        this.setState({ loginSuccessful: true });
+        this.props.setUser(result)
+    } else {
+        this.setState({ loginSuccessful: false });
+
+    }
+  }
 
   render() {
+    let loginMessage: any;
+    if (this.state.loginAttempted) {
+      if (this.state.loginSuccessful) {
+        loginMessage = <label>Login sucessful</label>;
+      } else {
+        loginMessage = <label>Login failed</label>;
+      }
+    }
+
     return (
       <div>
         <h1>Pleaselogin</h1>
-        <form>
-          <input value={this.state.userName} />
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            value={this.state.userName}
+            onChange={(e) => this.setUserName(e)}
+          />
           <br />
-          <input value={this.state.password} type="password" />
+          <input
+            value={this.state.password}
+            type="password"
+            onChange={(e) => this.setPassword(e)}
+          />
           <br />
+
           <input type="submit" value="Login" />
         </form>
+        {loginMessage}
       </div>
     );
   }
